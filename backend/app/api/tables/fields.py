2233af6772998b5e0a98529d46a2798304d8a6da
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-09-09 00:33:18 +0200

    attributs fields

--- backend/app/api/tables/fields.py
+++ backend/app/api/tables/fields.py
@@ -4,13 +4,8 @@ from flask_restful import fields
 get_fields = {
     "id": fields.Integer(),
     "name": fields.String(),
-    "primary_key": fields.Boolean(),
-    "index_key": fields.Boolean(),
-    "unique_kex": fields.Boolean(),
-    "size": fields.Integer(),
-    "type": fields.String(),
     "description": fields.String(),
     "created_at": fields.String(),
     "updated_at": fields.String(),
-    "table_id": fields.Integer(),
+    "project_id": fields.Integer()
 }
