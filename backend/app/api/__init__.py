daf3ad851a8039f8ae8a0cfa66ee6ea97916a3d7
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-09-12 02:51:21 +0200

    add tables to api

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,16 +2,19 @@ from flask_restful import Api
 from .projects.controllers import ProjectAllRessource, ProjectOneRessource
 from .users.controllers import UserAllResource, UserLoginResource, UserOneResource
 from .attributs.controllers import AttributAllRessource, AttributOneRessource
+from .tables.controllers import TablesAllRessource, TableOneRessource
 
 
 def make_api(app):
     """Create a Flask-Restx api, add some Ressources and add it to the app."""
     api = Api()
 
-    api.add_resource(ProjectAllRessource, "/v1/projects")
-    api.add_resource(ProjectOneRessource, "/v1/projects/<int:project_id>")
     api.add_resource(AttributAllRessource, "/v1/attributs")
     api.add_resource(AttributOneRessource, "/v1/attributs/<int:attrib_id>")
+    api.add_resource(ProjectAllRessource, "/v1/projects")
+    api.add_resource(ProjectOneRessource, "/v1/projects/<int:project_id>")
+    api.add_resource(TablesAllRessource, "/v1/tables")
+    api.add_resource(TableOneRessource, "/v1/tables/<int:table_id>")
     api.add_resource(UserAllResource, "/v1/users")
     api.add_resource(UserOneResource, "/v1/users/<int:project_id>")
     api.add_resource(UserLoginResource, "/v1/users/login")
