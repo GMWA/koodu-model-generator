274360d0dcc7687927ea218677207d09a4495954
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 11:26:41 +0200

    Users Schemas

---
+++ backend/modelgenerator/schemas/users.py
@@ -0,0 +1,31 @@
+from typing import List, Union
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+
+
+class UserCreate(UserBase):
+    description: Union[str, None]
+
+
+class UserUpdate(UserBase):
+    id: int
+    lastname: Union[str, None]
+    firstname: Union[str, None]
+    phone: Union[str, None]
+    password: Union[str, None]
+    is_verified = Union[bool, None]
+
+
+class User(UserBase):
+    id: int
+    lastname: Union[str, None]
+    firstname: Union[str, None]
+    phone: Union[str, None]
+    is_verified = Union[bool, None]
+    created_at: datetime
+    updated_at: datetime
