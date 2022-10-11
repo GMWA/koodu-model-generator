f0ef8864c7a9f8ecd6d5e3c6694783040a2fba1f
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-11 12:19:35 +0200

    Implement the main app

---
+++ backend/modelgenerator/routers/attributs.py
@@ -0,0 +1,66 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..schemas.attributs import (Attribut as AttributSchema,
+                                AttributCreate as AttributCreateSchema,
+                                AttributUpdate as AttributUpdateSchema)
+from ..models import Attribut as AttributModel
+from ..dependencies import get_db
+
+router = APIRouter(
+    prefix="/attributs",
+    tags=["attributs"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get(
+    "/",
+    response_model=List[AttributModel],
+    responses={403: {"description": "Operation forbidden"}}
+)
+async def read_attributs(db: Session = Depends(get_db)):
+    data = db.query(AttributModel).all()
+    return data
+
+
+@router.get(
+    "/{attribut_id}",
+    response_model=AttributModel,
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def get_attribut(attribut_id: int, db: Session = Depends(get_db)):
+    data = db.query(AttributModel).filter_by(id=attribut_id).first()
+    return data
+
+
+@router.post(
+    "/",
+    response_model=AttributSchema,
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def create_attribut(
+    attribut: AttributCreateSchema,
+    db: Session = Depends(get_db)
+):
+    return {}
+
+
+@router.put(
+    "/{attribut_id}",
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_attribut(
+    attribut_id: int,
+    attribut: AttributUpdateSchema,
+    db: Session = Depends(get_db)
+):
+    return {}
+
+
+@router.delete(
+    "/{attribut_id}",
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def delete_attribut(attribut_id: int, db: Session = Depends(get_db)):
+    return {}
---
+++ backend/modelgenerator/routers/users.py
@@ -0,0 +1,66 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..schemas.users import (User as UserSchema,
+                                UserCreate as UserCreateSchema,
+                                UserUpdate as UserUpdateSchema)
+from ..models import User as UserModel
+from ..dependencies import get_db
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get(
+    "/",
+    response_model=List[UserModel],
+    responses={403: {"description": "Operation forbidden"}}
+)
+async def read_users(db: Session = Depends(get_db)):
+    data = db.query(UserModel).all()
+    return data
+
+
+@router.get(
+    "/{user_id}",
+    response_model=UserModel,
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    data = db.query(UserModel).filter_by(id=user_id).first()
+    return data
+
+
+@router.post(
+    "/",
+    response_model=UserSchema,
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def create_user(
+    user: UserCreateSchema,
+    db: Session = Depends(get_db)
+):
+    return {}
+
+
+@router.put(
+    "/{user_id}",
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_user(
+    user_id: int,
+    user: UserUpdateSchema,
+    db: Session = Depends(get_db)
+):
+    return {}
+
+
+@router.delete(
+    "/{user_id}",
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    return {}
