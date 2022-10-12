b0b983a78138703b403aa3df781e35a2a2262f4f
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-12 12:22:12 +0200

    add supertoken nedeed package in the package.json

---
+++ frontend/src/pages/users/Auth.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>Auth view</div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ frontend/src/pages/users/ForgotPassword.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>Forgot Password</div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
