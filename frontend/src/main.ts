10addf2a8b1b95b817d46d5ef99fe9a6f4fce7c6
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-12 12:22:54 +0200

    init supertoken in the main app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from "vue"
-import "./style.css"
-import App from "./App.vue"
+import { createApp } from "vue";
+
+import axios from "axios";
+import SuperTokens from "supertokens-web-js";
+import Session from "supertokens-web-js/recipe/session";
+
+Session.addAxiosInterceptors(axios);
+
+import "./style.css";
+import App from "./App.vue";
+
+SuperTokens.init({
+  appInfo: {
+    appName: "modelgenerator",
+    apiDomain: "http://localhost:8000"
+  },
+  recipeList: [Session.init()]
+});
 
 createApp(App).mount("#app")
