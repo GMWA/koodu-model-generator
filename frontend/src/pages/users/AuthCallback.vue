c7ba25f19630b3ed891e43fbf02423278aea747c
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-14 23:10:59 +0200

    implement Authcallback

--- frontend/src/pages/users/AuthCallback.vue
+++ frontend/src/pages/users/AuthCallback.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+	import ThirdPartyEmailPassword from "supertokens-web-js/recipe/thirdpartyemailpassword";
+	import { onMounted } from "vue";
 
+	onMounted(async () => {
+    try {
+      const response = await ThirdPartyEmailPassword.thirdPartySignInAndUp({});
+      if (response.status !== "OK") {
+        return window.location.assign("/auth?error=signin");
+      }
+      window.location.assign("/");
+    } catch (_) {
+      window.location.assign("/auth?error=signin");
+    }
+  })
 </script>
 
 <template>
 	<div>
-		Forgot Password
+		<svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+			<!-- ... -->
+		</svg>
 	</div>
 </template>
 
