f2247ae0c320349a1020868a1ecf4a24cce7f180
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-15 16:23:46 +0200

    rename store files

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,4 @@
+export { useProjectStore }  from "./project.store";
+export { useTableStore }  from "./table.store";
+export { useUserStore } from "./user.store";
+export { useAttributStore } from "./attributs.store";
