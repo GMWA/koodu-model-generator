3fcf9cafcea32cd70b9f0786b9a6d88aea984eb9
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-16 12:10:52 +0200

    fix design on app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,41 @@
 </script>
 
 <template>
-   <div>
+   <div class="app">
     <Sidebar />
-    <router-view />
+    <main class="home-page">
+      <router-view />
+    </main>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+:root {
+	--primary: #4ade80;
+	--primary-alt: #22c55e;
+	--grey: #64748b;
+	--dark: #1e293b;
+	--dark-alt: #334155;
+	--light: #f1f5f9;
+	--sidebar-width: 300px;
+}
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
+}
+body {
+	background: var(--light);
+}
+.app {
+	display: flex;
+	main {
+		flex: 1 1 0;
+		padding: 2rem;
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
 </style>
