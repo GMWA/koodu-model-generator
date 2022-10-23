023642a67c06dccd77ed9595de8ffba2a97782b0
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-23 12:19:54 +0200

    update design on all pages

---
+++ frontend/src/components/ProjectCard.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+	<div>Project's Card</div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 	import Sidebar from "../components/Sidebar.vue";
 	import Footer from "../components/Footer.vue";
+	import Header from "../components/Header.vue";
 </script>
 
 <template>
 	<div class="flex">
 		<Sidebar />
-    <main class="home-page">
+    <main class="flex flex-col w-full">
+			<Header />
+			
 			<div>Dashboard Page</div>
 
 			<Footer />
