ff17cc9fbc3dfc02ac7e5f7b48ed9cda77cdeec0
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-25 01:25:16 +0200

    design project card

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
+	import { IProject } from "../types/projects.type";
 
+	interface ProjectProps {
+		project: IProject
+	}
+	const props = withDefaults(defineProps<ProjectProps>(), {});
+	const project = props.project;
 </script>
 
 <template>
-	<div>Project's Card</div>
+	<div
+		class="flex flex-col items-center
+		shadow shadow-inner hover:shadow-ls"
+	>
+		<div>
+			{{ project.name }}
+		</div>
+		<div>
+			{{ project.description }}
+		</div>
+	</div>
 </template>
 
 <style scoped>
