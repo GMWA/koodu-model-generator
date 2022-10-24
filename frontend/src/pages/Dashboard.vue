3ef1f7b09ccb3801ba0ab2ee5ae2f03d1ba85fe8
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-10-25 00:51:03 +0200

    design other pages with title and type props

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -8,7 +8,10 @@
 	<div class="flex">
 		<Sidebar />
     <main class="flex flex-col w-full">
-			<Header />
+			<Header
+				type="page"
+				title="Dashboard"
+			/>
 			
 			<div>Dashboard Page</div>
 
