5f2d3fffa22c5d218924b23e1f9c900a2890a3dd
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-04 00:59:48 +0100

    fix design in project modal

--- frontend/src/pages/Projects.vue
+++ frontend/src/pages/Projects.vue
@@ -50,8 +50,9 @@
 			<Footer />
     	</main>
 		<div
-			aria-hidden="false"
-			class="overflow-y-auto overflow-x-auto fixed flex items-center justify-center bg-gray-900 z-50 w-full md:inset-0 md:h-full"
+			class="overflow-y-auto overflow-x-auto fixed flex items-center justify-center
+				  bg-gray-900 bg-opacity-50 z-50 w-full md:inset-0 md:h-full
+				  transition duration-150 ease-in-out"
 			:class="{ hidden: is_add_btn_open }"
 		>
 			<div
@@ -62,7 +63,7 @@
 					<!-- Modal header -->
 					<div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
 						<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-							Terms of Service
+							Add Project
 						</h3>
 						<button @click="openModel" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
 							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
