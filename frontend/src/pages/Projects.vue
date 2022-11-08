274b2e7f09e030d61026bf1617a7e8d135580156
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-08 03:53:25 +0100

    fix: management of modals

--- frontend/src/pages/Projects.vue
+++ frontend/src/pages/Projects.vue
@@ -23,16 +23,27 @@
 		{id: 6, name: "Project 6", description: "Description of the project 6"},
 	];
 
-	const openAddModel = () => {
-		is_add_modal_open.value = !is_add_modal_open.value;
+	const openAddModal = () => {
+		is_add_modal_open.value = true;
+	}
+	const closeAddModal = () => {
+		is_add_modal_open.value = false;
 	}
 
-	const openEditModel = () => {
-		is_edit_modal_open.value = !is_edit_modal_open.value;
+	const openEditModal = (project: IProject) => {
+		toEditProject = project;
+		is_edit_modal_open.value = true;
+	}
+	const closeEditModal = () => {
+		is_edit_modal_open.value = false;
 	}
 
-	const openDeleteModel = () => {
-		is_delete_modal_open.value = !is_delete_modal_open.value;
+	const openDeleteModal = (project: IProject) => {
+		toDeleteProject = project;
+		is_delete_modal_open.value = true;
+	}
+	const closeDeleteModal = () => {
+		is_delete_modal_open.value = false;
 	}
 </script>
 
@@ -84,7 +95,7 @@
 						<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
 							Add Project
 						</h3>
-						<button @click="openAddModel" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+						<button @click="closeAddModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
 							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 							<span class="sr-only">Close modal</span>
 						</button>
@@ -131,7 +142,7 @@
 							Add
 						</button>
 						<button
-							@click="openAddModel"
+							@click="closeAddModal"
 							type="button"
 							class="text-gray-500 bg-white hover:bg-gray-100
 							focus:ring-4 focus:outline-none focus:ring-blue-300
@@ -164,7 +175,7 @@
 						<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
 							Edit Project
 						</h3>
-						<button @click="openEditModel" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+						<button @click="closeEditModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
 							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 							<span class="sr-only">Close modal</span>
 						</button>
@@ -211,7 +222,7 @@
 							Edit
 						</button>
 						<button
-							@click="openEditModel"
+							@click="closeEditModal"
 							type="button"
 							class="text-gray-500 bg-white hover:bg-gray-100
 							focus:ring-4 focus:outline-none focus:ring-blue-300
@@ -244,7 +255,7 @@
 						<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
 							Delete Project
 						</h3>
-						<button @click="openDeleteModel" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+						<button @click="closeDeleteModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
 							<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 							<span class="sr-only">Close modal</span>
 						</button>
@@ -267,7 +278,7 @@
 							Delete
 						</button>
 						<button
-							@click="openDeleteModel"
+							@click="closeDeleteModal"
 							type="button"
 							class="text-gray-500 bg-white hover:bg-gray-100
 							focus:ring-4 focus:outline-none focus:ring-blue-300
