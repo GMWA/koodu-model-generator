965e588b6cb9dd06563246c3640f0007fb1c6f08
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-16 12:12:13 +0100

    eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,20 +5,19 @@ module.exports = {
   root: true,
   parser: "vue-eslint-parser",
   parserOptions: {
-      parser: "@typescript-eslint/parser",
+    parser: "@typescript-eslint/parser",
   },
   extends: [
-      "plugin:vue/strongly-recommended",
-      "eslint:recommended",
-      "@vue/typescript/recommended",
-      "prettier",
+    //"plugin:vue/strongly-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:vue/recommended"
   ],
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
-      "prettier/prettier": "error",
-      // not needed for vue 3
-      "vue/no-multiple-template-root": "off",
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_', "argsIgnorePattern": "^_" }],
+    "prettier/prettier": "error",
+    // not needed for vue 3
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_', "argsIgnorePattern": "^_" }],
   },
 }
