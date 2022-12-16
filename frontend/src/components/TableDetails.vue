f3b2fe8dec6082e54167bc2a5a411ac62ca0defe
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-16 15:51:38 +0100

    Tables Details and components

---
+++ frontend/src/components/TableDetails.vue
@@ -0,0 +1,11 @@
+<script lang="ts" , setup>
+import { useRoute, useRouter } from "vue-router";
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div>Details</div>
+  </div>
+</template>
+
+<style scoped></style>
