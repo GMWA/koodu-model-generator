ce373cad7c55f482128a1356ccb8210ea5f8afde
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-17 15:56:24 +0100

    design list of table

--- frontend/src/components/TableDetails.vue
+++ frontend/src/components/TableDetails.vue
@@ -1,10 +1,22 @@
 <script lang="ts" , setup>
-import { useRoute, useRouter } from "vue-router";
+import { ITable } from "../types/tables.type";
+import { useAttributStore } from "../store/attributs.store";
+import { reactive } from "vue";
+
+interface TableProps {
+  table: ITable;
+}
+const props = withDefaults(defineProps<TableProps>(), {});
+const table = reactive(props.table);
+const emit = defineEmits(["delete", "edit", "choose"]);
+const attributs = useAttributStore();
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div>Details</div>
+    <div>{{ table.name }}</div>
+    <div>Select</div>
+    <div>Body</div>
   </div>
 </template>
 
