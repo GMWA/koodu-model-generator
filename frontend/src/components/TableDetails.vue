743d02d49ce159b4077a06480ce4fbf46ef0afad
Alex <mboutchouangalex+github@gmail.com>
2023-06-28 23:04:58 +0200

    delete Attribut

--- frontend/src/components/TableDetails.vue
+++ frontend/src/components/TableDetails.vue
@@ -71,8 +71,9 @@ const editAttribut = () => {
   console.log(selectedAttribut);
 };
 
-const deleteAttribut = () => {
-  console.log(toDeleteAttribut);
+const deleteAttribut = async () => {
+  await attributs.removeItem(toDeleteAttribut.value.id);
+  closeDeleteModal();
 };
 </script>
 
@@ -371,6 +372,7 @@ const deleteAttribut = () => {
             class="flex flex-row-reverse items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600"
           >
             <button
+              @click="deleteAttribut"
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
