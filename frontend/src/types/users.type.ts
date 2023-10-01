d7371d4f65414e3c75e2f15335d6d0e17c295d90
Alex <mboutchouangalex+github@gmail.com>
2023-10-01 21:44:08 +0200

    Frontend: save new user in the db when login

--- frontend/src/types/users.type.ts
+++ frontend/src/types/users.type.ts
@@ -2,10 +2,17 @@ export interface IUser  {
   id: number;
   email: string;
   username: string;
-  firstname?: string;
-  lastname?: string;
-  phone?: string;
-  is_verified: boolean,
-  created_at?: string;
-  updated_at?: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  is_admin: boolean;
+  thirdparty: string;
+  created_at: string;
+  updated_at: string;
 }
+
+export interface IRootTableState {
+  logged_user: IUser | null;
+  loading: boolean;
+  error: null | string;
+};
