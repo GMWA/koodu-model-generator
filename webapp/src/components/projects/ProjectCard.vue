d73abe88a78a75ef08be7a51cd911d3defb51401
Alex <mboutchouangalex+github@gmail.com>
2024-06-03 21:36:35 +0200

    init Projectcard

---
+++ webapp/src/components/projects/ProjectCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Project Card</h1>
+    <p>{{ props.project.name }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IProject } from '../../interfaces';
+
+const props =  defineProps({
+  project: {
+    type: Object as () => IProject,
+    required: true
+  }
+})
+</script>
