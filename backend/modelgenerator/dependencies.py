6bcace224e7675b739f9af03953f07e9379724fb
Alex <mboutchouangalex+github@gmail.com>
2024-06-03 23:17:00 +0200

    logged user dependency

--- backend/modelgenerator/dependencies.py
+++ backend/modelgenerator/dependencies.py
@@ -1,7 +1,18 @@
-from fastapi import HTTPException, status
-
+import os
+import jwt
+from fastapi import Depends, HTTPException, status
 from modelgenerator.database import SessionLocal
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from modelgenerator.models import User as UserModel
+from modelgenerator.schemas.tokens import TokenData
+from modelgenerator.schemas.users import User
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
 
 def get_db():
     db = SessionLocal()
@@ -10,3 +21,24 @@ def get_db():
     finally:
         db.close()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = UserModel.query.filter_by(email=token_data.username).first()
+    if user is None or not user.is_active:
+        raise credentials_exception
+    return user
