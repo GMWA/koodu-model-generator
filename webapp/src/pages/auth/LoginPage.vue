6b88f28a52bfd13a0c1ea9c203fd09549713ce41
Alex <mboutchouangalex+github@gmail.com>
2024-06-03 21:37:18 +0200

    init Auth Pages

---
+++ webapp/src/pages/auth/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Auth Page</h1>
+    <div v-if="true">
+      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button @click="login" :disabled="loading">Login</button>
+      <p v-if="error">{{ error }}</p>
+  </div>
+  <div v-else>
+    <p>Welcome, {{ 'Alex' }}</p>
+    <button @click="logout">Logout</button>
+  </div>
+
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+// import { useUserStore } from '../../stores/userStore'
+
+// const store = useUserStore()
+
+const email = ref('')
+const password = ref('')
+const error = ref('')
+const loading = ref(false)
+
+const login = async () => {
+  loading.value = true
+  try {
+    //await store.dispatch('auth/login', { email: email.value, password: password.value })
+  } catch (e) {
+    // error.value = e.message
+  } finally {
+    loading.value = false
+  }
+}
+
+const logout = async () => {
+  //await store.dispatch('auth/logout')
+}
+</script>
---
+++ webapp/src/pages/auth/RegistrationPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Registration Page</h1>
+    <div>
+      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button @click="register" :disabled="loading">Register</button>
+      <p v-if="error">{{ error }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+// import { useUserStore } from '../../stores/userStore'
+
+// const store = useUserStore()
+
+const email = ref('')
+const password = ref('')
+const error = ref('')
+
+const loading = ref(false)
+
+const register = async () => {
+  loading.value = true
+  try {
+    //await store.dispatch('auth/register', { email: email.value, password: password.value })
+  } catch (e) {
+    // error.value = e.message
+  } finally {
+    loading.value = false
+  }
+}
+</script>
