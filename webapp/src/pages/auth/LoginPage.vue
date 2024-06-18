5390d36db3a0e8440890a27dd152bd437eafd783
Alex <mboutchouangalex+github@gmail.com>
2024-06-18 23:52:12 +0200

    Login layout

--- webapp/src/pages/auth/LoginPage.vue
+++ webapp/src/pages/auth/LoginPage.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <h1>Auth Page</h1>
-    <div v-if="true">
-      <input v-model="email" type="text" placeholder="Email" />
-      <input v-model="password" type="password" placeholder="Password" />
-      <button @click="login" :disabled="loading">Login</button>
-      <p v-if="error">{{ error }}</p>
-  </div>
-  <div v-else>
-    <p>Welcome, {{ 'Alex' }}</p>
-    <button @click="logout">Logout</button>
-  </div>
-
+  <div class="pcontainer">
+    <div class="row bcontainer">
+      <div class="col colum dcontainer">
+        <h5>Login hier to quickly to manage your projects</h5>
+        <div class="login-card">
+        <q-form @submit="login" @reset="reset" class="w-full">
+          <q-input
+            outlined
+            v-model="email"
+            label="Email"
+            type="email"
+            class="w-full q-pb-md"
+          />
+          <q-input
+            outlined
+            v-model="password"
+            label="Password" type="password" class="w-full"
+            :rules="[val => val && val.length > 0 || 'Please enter your password', validatePassword]"
+          />
+          <div class="w-full flex flex-row items-center q-pb-md">
+            <div class="flex col justify-start">
+              <q-checkbox class="" v-model="remember" label="Remember me" />
+            </div>
+            <div class="flex flex-col-reverse justify-end">
+              <router-link class="" to="/forgot-password">Forgot password?</router-link>
+            </div>
+          </div>
+          <q-btn class="w-full q-p-md" label="Login" type="submit" color="primary" :disabled="loading"/>
+          <p v-if="error">{{ error }}</p>
+        </q-form>
+      </div>
+        <p>Already have an account? <router-link to="/login">Register</router-link></p>
+      </div>
+      <div class="col colum q-gutter-md" style="min-width: 400px;">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,7 +48,15 @@ import { ref } from 'vue'
 const email = ref('')
 const password = ref('')
 const error = ref('')
-const loading = ref(false)
+const loading = ref(false);
+const remember = ref(false)
+
+/*const links = [
+  { title: 'About Us', url: '/about' },
+  { title: 'Contact Us', url: '/contact' },
+  { title: 'Privacy Policy', url: '/privacy' },
+  { title: 'Terms of Service', url: '/terms' }
+]*/
 
 const login = async () => {
   loading.value = true
@@ -38,7 +69,35 @@ const login = async () => {
   }
 }
 
-const logout = async () => {
-  //await store.dispatch('auth/logout')
+const reset = () => {
+  email.value = ''
+  password.value = ''
+}
+
+const validatePassword = (val: string) => {
+  // at least 8 characters, 1 upper case letter, 1 lowercase letter, 1 number and 1 special character
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(val)
 }
 </script>
+
+<style scoped lang="scss">
+.dcontainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: #f5f5f5;
+}
+.login-card {
+  min-width: 600px;
+  padding: 20px;
+
+}
+.w-full {
+  width: 100%;
+}
+.item {
+  border: 2px solid red;
+}
+</style>
