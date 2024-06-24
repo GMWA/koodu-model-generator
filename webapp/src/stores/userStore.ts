fc48c7bcce1fa09a985535a4f903de7db760571a
Alex <mboutchouangalex+github@gmail.com>
2024-06-24 22:50:00 +0200

    user store

--- webapp/src/stores/userStore.ts
+++ webapp/src/stores/userStore.ts
@@ -1,19 +1,37 @@
+import { api } from '../boot/axios';
 import { defineStore } from 'pinia';
+import { ICreateUser, IUser, IAccessToken } from 'src/interfaces';
+import { AuthEndpoint } from 'src/constants/endpoints';
 
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     isLoggedIn: false,
-    user: null,
+    user: null as IUser | null,
+    token: null as IAccessToken | null,
   }),
   actions: {
-    login(username: string, password: string) {
-      // Perform login logic here
-      // Example: make an API call to authenticate the user
-      // If successful, update the store state
+    async login(username: string, password: string): Promise<IAccessToken> {
+      const response = await api.post<IAccessToken>(AuthEndpoint.LOGIN, { username, password });
+      const token = response.data;
       this.isLoggedIn = true;
-      // this.user = { username };
-      console.log('User logged in:', this.user, password);
+      this.token = token;
+      console.log('User logged in', token);
+      return token;
+    },
+    async register(newUser: ICreateUser): Promise<IUser> {
+      const response = await api.post<IUser>(AuthEndpoint.REGISTER, newUser);
+      const user = response.data;
+      console.log('User registered', user);
+      return user;
+    },
+    async fetchUser() {
+      const response = await api.get<IUser>(AuthEndpoint.ME);
+      const user = response.data;
+      this.isLoggedIn = true;
+      this.user = user;
+      console.log('User fetched', user);
+      return user;
     },
     logout() {
       // Perform logout logic here
