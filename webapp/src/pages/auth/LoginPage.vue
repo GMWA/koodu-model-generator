b37fb0c09431ce8eb9dac7be7ef6588c39e1c3f5
Alex <mboutchouangalex+github@gmail.com>
2024-06-24 22:34:41 +0200

    login and registration forms

--- webapp/src/pages/auth/LoginPage.vue
+++ webapp/src/pages/auth/LoginPage.vue
@@ -2,25 +2,30 @@
   <div class="pcontainer">
     <div class="row bcontainer">
       <div class="col colum dcontainer">
-        <h3>Login</h3>
+        <h1 class="text-primary">Login</h1>
         <div class="login-card">
           <q-form @submit="login" @reset="reset" class="w-full">
             <q-input outlined v-model="email" label="Email" type="email" class="w-full q-ma-md" />
-            <q-input outlined v-model="password" label="Password" type="password" class="w-full q-ma-md"
-              :rules="[val => val && val.length == 0 || 'Please enter your password', validatePassword]" />
+            <q-input outlined v-model="password" :rules="[val => val.length > 0 || 'Please enter your password']"
+              :type="isPwd ? 'password' : 'text'" label="Password" class="w-full q-ma-md">
+              <template v-slot:append>
+                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                  @click="isPwd = !isPwd" />
+              </template>
+            </q-input>
             <div class="w-full flex flex-row items-center q-pb-md q-ma-md">
               <div class="flex col justify-start">
-                <q-checkbox class="" v-model="remember" label="Remember me" />
+                <q-checkbox class="text-primary" v-model="remember" label="Remember me" />
               </div>
               <div class="flex flex-col-reverse justify-end">
-                <router-link class="" to="/forgot-password">Forgot password?</router-link>
+                <router-link class="text-primary" to="/forgot-password">Forgot password?</router-link>
               </div>
             </div>
             <q-btn class="w-full q-pa-md q-ma-md" label="Login" type="submit" color="primary" :disabled="loading" />
             <p v-if="error">{{ error }}</p>
           </q-form>
         </div>
-        <p>Already have an account? <router-link to="/registration">Register</router-link></p>
+        <p>Already have an account? <router-link class="text-primary" to="/registration">Register</router-link></p>
       </div>
       <div class="col colum bg-primary" style="min-width: 400px;">
       </div>
@@ -31,13 +36,13 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-// import { useUserStore } from '../../stores/userStore'
+import { useUserStore } from '../../stores/userStore'
 
-// const store = useUserStore()
-
+const authStore = useUserStore()
 const email = ref('')
 const password = ref('')
 const error = ref('')
+const isPwd = ref(true)
 const loading = ref(false);
 const remember = ref(false)
 
@@ -51,7 +56,11 @@ const remember = ref(false)
 const login = async () => {
   loading.value = true
   try {
-    //await store.dispatch('auth/login', { email: email.value, password: password.value })
+    const tokenInfos = await authStore.login(email.value, password.value);
+    if (tokenInfos) {
+      // router.push('/')
+      console.log('Login successful')
+    }
   } catch (e) {
     // error.value = e.message
   } finally {
@@ -64,10 +73,10 @@ const reset = () => {
   password.value = ''
 }
 
-const validatePassword = (val: string) => {
+/*const validatePassword = (val: string) => {
   // at least 8 characters, 1 upper case letter, 1 lowercase letter, 1 number and 1 special character
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(val)
-}
+}*/
 </script>
 
 <style scoped lang="scss">
