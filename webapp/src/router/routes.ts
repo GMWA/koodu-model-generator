26e710f1f975b8d980610c7e5fb577287490f355
Alex <mboutchouangalex+github@gmail.com>
2024-07-13 10:32:42 +0200

    project view page

--- webapp/src/router/routes.ts
+++ webapp/src/router/routes.ts
@@ -2,12 +2,6 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/projects/ProjectListPage.vue') }],
-    meta: { requiresAuth: true },
-  },
-  {
     path: '/auth/login',
     component: () => import('layouts/AuthLayout.vue'),
     children: [{ path: '', name: 'LoginPage', component: () => import('pages/auth/LoginPage.vue') }],
@@ -36,6 +30,12 @@ const routes: RouteRecordRaw[] = [
     path: '/projects',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/projects/ProjectListPage.vue') }],
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/project-view/:id',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/projects/ProjectViewPage.vue') }],
     meta: { requiresAuth: true },
   },
 
