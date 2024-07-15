2068f27aa27222b76abaddfde53b942845c09443
Alex <mboutchouangalex+github@gmail.com>
2024-07-15 22:01:32 +0200

    add IAttributTypes

---
+++ webapp/src/constants/attributs.ts
@@ -0,0 +1,13 @@
+import { IAttributType } from '../interfaces';
+
+
+export const attributTypes: IAttributType[] = [
+  {name: 'String', value: 'str'},
+  {name: 'Integer', value: 'int'},
+  {name: 'Decimal', value: 'float'},
+  {name: 'Boolean', value: 'bool'},
+  {name: 'DateTime', value: 'datetime'},
+  {name: 'Time', value: 'time'},
+  {name: 'Date', value: 'date'},
+  {name: 'Reference', value: 'ref'}
+]
